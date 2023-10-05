179eb21f7e6b27b11f8f5933bd7be3d7f40723d2
Franchesco32 <143191063+Franchesco32@users.noreply.github.com>
2023-10-04 21:44:31 -0300

    Create card.js

---
+++ card.js
@@ -0,0 +1,24 @@
+const giro = document.querySelector(".card")
+
+        giro.addEventListener('mousemove',(event)=>{
+            const mouseX = event.clientX - giro.getBoundingClientRect().left;
+
+            const mouseY = event.clientY - giro.getBoundingClientRect().top;
+
+            const centerX = giro.clientWidth / 2;
+
+            const centerY = giro.clientHeight / 2;
+
+            const rotationX = (mouseY - centerY) /10;
+
+            const rotationY = (mouseX - centerX) / 10;
+// mover mais para tras ou para frente(quanto + px no [perspective(00px)] menos se movimenta)
+            giro.style.transform = `perspective(800px) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+
+            // giro.style.cursor = 'pointer';
+
+        });
+
+        giro.addEventListener('mouseleave', ()=>{
+            giro.style.transform = 'perspective(100px) rotateX(0deg) rotateY(0deg)'
+        });
